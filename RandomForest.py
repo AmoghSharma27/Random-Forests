fe725ea3975b4f8385354da1687f73e5a82ab926
Dhruv Sharma <dhruvsh704@gmail.com>
2026-03-31 14:40:12 -0300

    Added very basic functions and taken input

--- RandomForest.py
+++ RandomForest.py
@@ -2,8 +2,20 @@ import numpy as np
 import pandas as pd
 import matplotlib as mtl
 
-def main():
+def run_random_forest(df):
+    #TODO: Implement random forest algorithm
     pass
+
+def format_properly(result):
+    #TODO: Print output properly based on result
+    pass
+
+def main():
+    df = pd.read_csv("./database/All_Pokemon.csv")
+
+    result = run_random_forest(df)
+
+    print(format_properly(result))
 
 if __name__ == '__main__':
     main()
